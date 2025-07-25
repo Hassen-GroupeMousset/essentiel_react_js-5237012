e31f6c4b879d4c81004c72457f4a32e5189cf1b0
Hassen Malek <hassen.malek@jetransporte.com>
2025-07-25 15:57:10 +0200

    context.provider

---
+++ fichiers-exercice/05/05_02/src/context.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const todosContext = createContext()
+
+const todosProvider = (children) => {
+    cont [theme, setTheme] = iseState('darck')
+    return <todosContext.Provider>{children}</todosContext.Provider>
+
+}
